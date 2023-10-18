ee8928d1ad30a609dee3a03bde387bab4fe677b0
Arini Arumsari <ariniarum98@gmail.com>
2023-10-18 23:14:57 +0700

    Add Submission Dicoding

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Submission Dicoding "Belajar Data Analytics dengan Python"
+# Submission Dicoding "Belajar Analisis Data dengan Python"
 
-## Project Data Analytics
+## Preview
+![Bike Sharing Dashboard Streamlit Preview](https://raw.githubusercontent.com/ariniamsr/bike-sharing/main/preview.png)
+
+## Project Analisis Data
 
 Repository ini berisi proyek data analytics yang saya kerjakan. Deployment in **Streamlit** <img src="https://user-images.githubusercontent.com/7164864/217935870-c0bc60a3-6fc0-4047-b011-7b4c59488c91.png" alt="Streamlit logo"></img>
 
@@ -10,16 +13,16 @@ Proyek ini bertujuan untuk menganalisis data pada E-Commerce Public Dataset. Tuj
 
 ## Struktur Direktori
 
-- **/dataset**: Direktori ini berisi data yang digunakan dalam proyek, dalam format .csv .
-- **/streamlit**: Direktori ini berisi main.py yang digunakan untuk membuat dashboard hasil analisis data.
-- **Proyek_Analisis_Data.ipynb**: File ini yang digunakan untuk melakukan analisis data.
+- **/data**: Direktori ini berisi data yang digunakan dalam proyek, dalam format .csv .
+- **/dashboard**: Direktori ini berisi main.py yang digunakan untuk membuat dashboard hasil analisis data.
+- **notebook.ipynb**: File ini yang digunakan untuk melakukan analisis data.
 
 ## Instalasi
 
 1. Clone repository ini ke komputer lokal Anda menggunakan perintah berikut:
 
    ```shell
-   git clone https://github.com/Hanifanta/Project-data-analytics.git
+   git clone https://github.com/ariniamsr/bike-sharing.git
    ```
 
 2. Pastikan Anda memiliki lingkungan Python yang sesuai dan pustaka-pustaka yang diperlukan. Anda dapat menginstal pustaka-pustaka tersebut dengan menjalankan perintah berikut:
@@ -33,10 +36,10 @@ Proyek ini bertujuan untuk menganalisis data pada E-Commerce Public Dataset. Tuj
 1. Masuk ke direktori proyek (Local):
 
     ```shell
-    cd Project-data-analytics/streamlit/
-    streamlit run main.py
+    cd bike-sharing/dashboard/
+    streamlit run dashboard.py
     ```
-    Atau bisa dengan kunjungi website ini [Project Data Analytics](https://data-analytics-project.streamlit.app/)
 
 ## Kontribusi
 Anda dapat berkontribusi pada proyek ini dengan melakukan pull request. Pastikan untuk menjelaskan perubahan yang Anda usulkan secara jelas dan menyeluruh.
+
---
+++ dashboard/dashboard.py
@@ -0,0 +1,201 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+sns.set(style='dark')
+
+
+
+def get_total_count_by_hour_df(hour_df):
+  hour_count_df =  hour_df.groupby(by="hours").agg({"count_cr": ["sum"]})
+  return hour_count_df
+
+
+def count_by_day_df(day_df):
+    day_df_count_2011 = day_df.query(str('dteday >= "2011-01-01" and dteday < "2012-12-31"'))
+    return day_df_count_2011
+
+
+def total_registered_df(day_df):
+   reg_df =  day_df.groupby(by="dteday").agg({
+      "registered": "sum"
+    })
+   reg_df = reg_df.reset_index()
+   reg_df.rename(columns={
+        "registered": "register_sum"
+    }, inplace=True)
+   return reg_df
+
+def total_casual_df(day_df):
+   cas_df =  day_df.groupby(by="dteday").agg({
+      "casual": ["sum"]
+    })
+   cas_df = cas_df.reset_index()
+   cas_df.rename(columns={
+        "casual": "casual_sum"
+    }, inplace=True)
+   return cas_df
+
+def sum_order (hour_df):
+    sum_order_items_df = hour_df.groupby("hours").count_cr.sum().sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+
+def macem_season (day_df): 
+    season_df = day_df.groupby(by="season").count_cr.sum().reset_index() 
+    return season_df
+
+
+days_df = pd.read_csv("day_clean.csv")
+hours_df = pd.read_csv("hour_clean.csv")
+
+datetime_columns = ["dteday"]
+days_df.sort_values(by="dteday", inplace=True)
+days_df.reset_index(inplace=True)   
+
+hours_df.sort_values(by="dteday", inplace=True)
+hours_df.reset_index(inplace=True)
+
+ 
+for column in datetime_columns:
+    days_df[column] = pd.to_datetime(days_df[column])
+    # hours_df[column] = pd.to_datetime(hours_df[column])
+
+
+
+min_date_days = days_df["dteday"].min()
+max_date_days = days_df["dteday"].max()
+
+
+min_date_hour = hours_df["dteday"].min()
+max_date_hour = hours_df["dteday"].max()
+
+
+ 
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image1_hH9B4gs.jpg")
+    
+        # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date_days,
+        max_value=max_date_days,
+        value=[min_date_days, max_date_days])
+  
+
+main_df_days = days_df[(days_df["dteday"] >= str(start_date)) & 
+                       (days_df["dteday"] <= str(end_date))]
+
+
+main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) & 
+                        (hours_df["dteday"] <= str(end_date))]
+
+hour_count_df = get_total_count_by_hour_df(main_df_hour)
+day_df_count_2011 = count_by_day_df(main_df_days)
+reg_df = total_registered_df(main_df_days)
+cas_df = total_casual_df(main_df_days)
+sum_order_items_df = sum_order(main_df_hour)
+season_df = macem_season(main_df_hour)
+
+#Melengkapi Dashboard dengan Berbagai Visualisasi Data
+
+st.header('Bike Sharing :sparkles:')
+
+st.subheader('Daily Sharing')
+
+col1, col2, col3 = st.columns(3)
+ 
+
+with col1:
+    total_orders = day_df_count_2011.count_cr.sum()
+    st.metric("Total Sharing Bike", value=total_orders)
+
+
+with col2:
+    total_sum = reg_df.register_sum.sum()
+    st.metric("Total Registered", value=total_sum)
+
+
+with col3:
+    total_sum = cas_df.casual_sum.sum()
+    st.metric("Total Casual", value=total_sum)
+
+st.subheader("Performa penjualan perusahaan dalam beberapa tahun terakhir")
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    days_df["dteday"],
+    days_df["count_cr"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+ 
+st.pyplot(fig)
+
+
+
+st.subheader("pada jam berapa yang paling banyak dan paling sedikit disewa?")
+ 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+ 
+
+ 
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Hours (PM)", fontsize=30)
+ax[0].set_title("Jam dengan banyak penyewa sepeda", loc="center", fontsize=30)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+ 
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Hours (AM)",  fontsize=30)
+ax[1].set_title("Jam dengan sedikit penyewa sepeda", loc="center", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+ 
+st.pyplot(fig)
+
+
+st.subheader("musim apa yang paling banyak disewa?")
+
+colors = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#90CAF9"]
+fig, ax = plt.subplots(figsize=(20, 10))
+sns.barplot(
+        y="count_cr", 
+        x="season",
+        data=season_df.sort_values(by="season", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+ax.set_title("Grafik Antar Musim", loc="center", fontsize=50)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='x', labelsize=35)
+ax.tick_params(axis='y', labelsize=30)
+st.pyplot(fig)
+
+#############################################
+
+st.subheader("Perbandingan Customer yang Registered dengan casual")
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+labels = 'casual', 'registered'
+sizes = [18.8, 81.2]
+explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',colors=["#D3D3D3", "#90CAF9"],
+        shadow=True, startangle=90)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig1)
+
